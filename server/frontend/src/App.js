96f50b2c4c787a45563b02403fccc9fcf9ec04e6
name <jrgomez2376@gmail.com>
2025-05-26 19:35:16 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,31 +1,18 @@
-//Added new Code Here
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // ✅ import Register
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={<RegisterPanel />} />
-        {/* Add more routes here later, like Register, Home, etc. */}
+        <Route path="/register" element={<Register />} /> {/* ✅ add Register route */}
+        {/* Add more routes here later, like Home, etc. */}
       </Routes>
     </Router>
   );
 }
 
 export default App;
-
-
-//import LoginPanel from "./components/Login/Login"
-//import { Routes, Route } from "react-router-dom";
-
-//function App() {
-  //return (
-    //<Routes>
-      //<Route path="/login" element={<LoginPanel />} />
-    //</Routes>
-  //);
-//}
-//export default App;
