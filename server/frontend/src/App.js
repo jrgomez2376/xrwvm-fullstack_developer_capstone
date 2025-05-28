f0fd9fe257774fef404c54de4ce70aaf39f558b0
name <jrgomez2376@gmail.com>
2025-05-27 21:38:21 -0400

    Adding temporary changes to Github

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,18 +1,42 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPanel from "./components/Login/Login";
-import Register from "./components/Register/Register"; // ✅ import Register
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";   // <-- import Home component
+import About from "./components/About/About"; // <-- import About if you have it
+import Contact from "./components/Contact/Contact"; // <-- import Contact if you have it
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPanel />} />
-        <Route path="/register" element={<Register />} /> {/* ✅ add Register route */}
-        {/* Add more routes here later, like Home, etc. */}
-      </Routes>
-    </Router>
-  );
+//New Code added 5.27.25
+// Try importing optional components
+let Home, About, Contact;
+try {
+  Home = require("./components/Home/Home").default;
+} catch (e) {
+  Home = () => <div>Home page coming soon...</div>;
+}
+try {
+  About = require("./components/About/About").default;
+} catch (e) {
+  About = () => <div>About page coming soon...</div>;
+}
+try {
+  Contact = require("./components/Contact/Contact").default;
+} catch (e) {
+  Contact = () => <div>Contact page coming soon...</div>;
 }
 
-export default App;
+//Delted Route to "home"
+function App() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/login" element={<LoginPanel />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+  export default App;
